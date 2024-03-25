4a1eaaea3524d4d6af910768597b7c36cb389714
Kauê Alcantara Rocha <118750523+Kaueroch@users.noreply.github.com>
2024-03-24 23:40:38 -0300

    Add files via upload

---
+++ execeções.java
@@ -0,0 +1,16 @@
+package estudos;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class execeções {
+	public static void main(String[] arg) {
+		for (int i = 0; i <= 5; i++) {
+			for (int x = 0; x <= i; x++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
